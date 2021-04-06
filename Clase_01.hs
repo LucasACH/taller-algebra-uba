9504169690f0ecee3c920f3ad120679830e65333
Lucas Achaval <achaval.lucas@gmail.com>
2021-04-06 11:45:06 -0300

    Ejercicio 3 - Guia 1

--- Clase_01.hs
+++ Clase_01.hs
@@ -1,14 +1,25 @@
 -- Ejercicios primera clase https://campus.exactas.uba.ar/pluginfile.php/267823/mod_resource/content/1/clase01.pdf
 
+-- Funciones base
+
+maximo :: Int -> Int -> Int 
+maximo x y | x > y = x
+           | otherwise = y
+
 
 -- (1) absoluto: calcula el valor absoluto de un número entero.
 
 absoluto :: Int -> Int 
-absoluto n | n < 0 = -n
-           | otherwise = n
+absoluto n = maximo n (-n)
 
--- (2) maximoabsoluto: devuelve el m´aximo entre el valor absoluto de dos n´umeros enteros.
+
+-- (2) maximoabsoluto: devuelve el máximo entre el valor absoluto de dos números enteros.
 
 maximoabsoluto :: Int -> Int -> Int
-maximoabsoluto x y | absoluto x > absoluto y = absoluto x
-                   | otherwise = absoluto y
+maximoabsoluto x y = maximo (absoluto x) (absoluto y)
+
+
+-- (3) maximo3: devuelve el máximo entre tres números enteros.
+
+maximo3 :: Int -> Int -> Int -> Int
+maximo3 x y z = maximo z (maximo x y)
