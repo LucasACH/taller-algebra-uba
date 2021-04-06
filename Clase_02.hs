5ad9a174f53018ec048112178df623126834d387
Lucas Achaval <achaval.lucas@gmail.com>
2021-04-06 14:07:17 -0300

    Ejercicio 6 | Clase 2

--- Clase_02.hs
+++ Clase_02.hs
@@ -33,6 +33,11 @@ sumaTerna (px, py, pz) = px + py + pz
 
 -- (6) posicPrimerPar: dada una terna de enteros, devuelve la posición del primer número par si es que hay alguno, y devuelve 4 si son todos impares.
 
+posicPrimerPar :: Integral a => (a, a, a) -> Int 
+posicPrimerPar (px, py, pz) | px `mod` 2 == 0 = 1
+                            | py `mod` 2 == 0 = 2
+                            | pz `mod` 2 == 0 = 3
+                            | otherwise = 4
 
 
 -- (7) crearPar :: a -> b -> (a, b): crea un par a partir de sus dos componentes dadas por separado (debe funcionar para elementos de cualquier tipo).
