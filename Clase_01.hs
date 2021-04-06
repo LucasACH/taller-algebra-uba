5b264435b9a89342e5a01dda98e18799106e0c06
Lucas Achaval <achaval.lucas@gmail.com>
2021-04-05 23:32:00 -0300

    Ejercicio 2 | Clase 1

--- Clase_01.hs
+++ Clase_01.hs
@@ -1,8 +1,14 @@
 -- Ejercicios primera clase https://campus.exactas.uba.ar/pluginfile.php/267823/mod_resource/content/1/clase01.pdf
 
+
 -- (1) absoluto: calcula el valor absoluto de un número entero.
 
 absoluto :: Int -> Int 
 absoluto n | n < 0 = -n
            | otherwise = n
 
+-- (2) maximoabsoluto: devuelve el m´aximo entre el valor absoluto de dos n´umeros enteros.
+
+maximoabsoluto :: Int -> Int -> Int
+maximoabsoluto x y | absoluto x > absoluto y = absoluto x
+                   | otherwise = absoluto y
