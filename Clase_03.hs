ec42a6627e1e8d61dc29d4a4720f19755337acf8
Lucas Achaval <achaval.lucas@gmail.com>
2021-04-06 14:59:51 -0300

    Ejercicio 2 | Clase 3

--- Clase_03.hs
+++ Clase_03.hs
@@ -5,9 +5,12 @@ esMultiplo3 n  | n == 3 = True
                | n < 3 = False
                | otherwise = esMultiplo3 (n - 3) 
                -- La función se sigue ejecutando hasta llegar a 3, si se cumple que n es multipo de 3
-               
+
 
 -- (2) Implementar la función sumaImpares :: Int -> Int que dado n ∈ N sume los primeros n números impares. Ej: sumaImpares 3 1+3+5 9.
+
+sumaImpares :: Int -> Int
+sumaImpares n = n^2
 
 
 -- (3) Escribir una función medioFact que dado n ∈ N calcula n!! = n (n − 2)(n − 4) · · · . Por ejemplo: medioFact 10 10 ∗ 8 ∗ 6 ∗ 4 ∗ 2 3840. medioFact 9 9 ∗ 7 ∗ 5 ∗ 3 ∗ 1 945.
