75f2aedb19b9ba4307d6a31d0012e969146116a3
Lucas Achaval <achaval.lucas@gmail.com>
2021-04-06 15:26:43 -0300

    Ejercicio 3 | Clase 3

--- Clase_03.hs
+++ Clase_03.hs
@@ -15,6 +15,9 @@ sumaImpares n | n == 1 = 1
 
 -- (3) Escribir una función medioFact que dado n ∈ N calcula n!! = n (n − 2)(n − 4) · · · . Por ejemplo: medioFact 10 10 ∗ 8 ∗ 6 ∗ 4 ∗ 2 3840. medioFact 9 9 ∗ 7 ∗ 5 ∗ 3 ∗ 1 945.
 
+medioFact :: (Ord a, Num a) => a -> a
+medioFact n | n <= 1 = 1
+            | otherwise = n * medioFact(n - 2)
 
 
 -- (4) Escribir una función que determine la suma de dígitos de un número positivo. Para esta función pueden utilizar div y mod.
