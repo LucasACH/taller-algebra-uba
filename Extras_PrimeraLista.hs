4e5cf3c69d3fd9dd831fa182ad91f0a1a2a03897
Lucas Achaval <achaval.lucas@gmail.com>
2021-04-08 18:22:44 -0300

    Ejercicio 6 | Primera Lista

--- Extras_PrimeraLista.hs
+++ Extras_PrimeraLista.hs
@@ -11,8 +11,8 @@ digito i n = (n `mod` 10^i) `div` 10^(i-1)
 -- negativo.
 
 sumaDeDigitos :: Integer -> Integer
-sumaDeDigitos i | i < 10 = i
-                | otherwise = i `mod` 10 + sumaDeDigitos(i `div` 10)
+sumaDeDigitos n | n < 10 = n
+                | otherwise = n `mod` 10 + sumaDeDigitos(n `div` 10)
 
 
 -- (3) Escriba una función dígitoMáximo :: Integer -> Integer tal que la expresión
@@ -25,9 +25,31 @@ sumaDeDigitos i | i < 10 = i
 -- factorial del entero no negativo n
 
 factorial :: Integer -> Integer
-factorial i | i == 0 = 1
-            | i > 0 = i * factorial (i-1)
+factorial n | n == 0 = 1
+            | n > 0 = n * factorial (n-1)
 
 
 -- (5) Escriba una función e_approx :: Integer -> Double tal que para cada entero no
 -- negativo n el valor de e_approx n sea
+
+factorialDouble :: Double -> Double
+factorialDouble n | n == 0 = 1
+                  | n > 0 = n * factorialDouble (n-1)
+
+e_approx :: Double -> Double
+e_approx n | n == 0 = 1
+           | otherwise = 1 / factorialDouble n + e_approx(n-1)
+
+
+-- (6) Escriba una función unos :: Integer -> Integer tal que para cada entero no
+-- negativo n el valor de unos n sea la cantidad de dígitos 1 en la escritura binaria
+-- de n.
+
+unos :: Integer -> Integer
+unos 1 = 1
+unos n | n < 1 = 0
+       | mod2 == 0 = unos(div2)
+       | otherwise = 1 + unos(div2)
+
+       where div2 = n `div` 2
+             mod2 = n `mod` 2
