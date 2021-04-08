d3b89ee6ee6132a8fcda6ff68ea01002b60751e9
Lucas Achaval <achaval.lucas@gmail.com>
2021-04-08 15:57:26 -0300

    Ejercicio 4 | Primera Lista

--- Extras_PrimeraLista.hs
+++ Extras_PrimeraLista.hs
@@ -17,3 +17,17 @@ sumaDeDigitos i | i < 10 = i
 
 -- (3) Escriba una función dígitoMáximo :: Integer -> Integer tal que la expresión
 -- dígitoMáximo n sea dígito más grande de n, que asumimos es no negativo.
+
+-- digitoMaximo :: Integer -> Integer
+
+
+-- (4) Escriba una función factorial :: Integer -> Integer tal que factorial n sea el
+-- factorial del entero no negativo n
+
+factorial :: Integer -> Integer
+factorial i | i == 0 = 1
+            | i > 0 = i * factorial (i-1)
+
+
+-- (5) Escriba una función e_approx :: Integer -> Double tal que para cada entero no
+-- negativo n el valor de e_approx n sea
