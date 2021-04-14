9987d996b01efd37cabf96e090349432c62db8a5
Lucas Achaval <achaval.lucas@gmail.com>
2021-04-14 11:55:45 -0300

    Ejercicio 3 | Clase 04

--- Clase_04.hs
+++ Clase_04.hs
@@ -8,4 +8,11 @@ f1 n = 1 - 2*(1-2^n)
 -- (2) sumatoria sobre i, desde 1 hasta n, de q potencia-i
 
 f2 :: (Integral a, Integral b) => b -> a -> a
-f2 n q = (q - q^(n + 1)) `div` (1 - q) 
+f2 n q = q * (1 - q^n) `div` (1 - q) 
+
+
+-- (3) sumatoria sobre i, desde 1 hasta 2n, de q potencia-i
+
+f3 :: (Integral a, Integral b) => b -> a -> a
+f3 n q = f2 x q
+       where x = 2 * n
