30f01ec5980d78586f2798e0fbc1708183923e79
Lucas Achaval <achaval.lucas@gmail.com>
2021-04-19 20:56:01 -0300

    Ejercicio 1 y 2 | Clase 04

--- Clase_04.hs
+++ Clase_04.hs
@@ -1,18 +1,26 @@
+-- Funciones base
+
+sumatoria :: Int -> Int 
+sumatoria 0 = 0
+sumatoria n = n + sumatoria (n-1)
+
+
 -- (1) sumatoria sobre i, desde 0 hasta n, de 2 potencia-i
 
-
-f1 :: (Integral b, Num a) => b -> a
-f1 n = 1 - 2*(1-2^n)
+f1 :: Int -> Int 
+f1 0 = 1
+f1 n = 2^n + f1(n-1)
 
 
 -- (2) sumatoria sobre i, desde 1 hasta n, de q potencia-i
 
-f2 :: (Integral a, Integral b) => b -> a -> a
-f2 n q = q * (1 - q^n) `div` (1 - q) 
+
 
 
 -- (3) sumatoria sobre i, desde 1 hasta 2n, de q potencia-i
 
-f3 :: (Integral a, Integral b) => b -> a -> a
-f3 n q = f2 x q
-       where x = 2 * n
+
+
+
+-- (4) sumatoria sobre i, desde n hasta 2n, de q potencia-i
+
