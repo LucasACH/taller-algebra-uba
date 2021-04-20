f58b7d42952baba371f2ff4510283d24c637f773
Lucas Achaval <achaval.lucas@gmail.com>
2021-04-19 21:27:19 -0300

    Ejercicio 4 | Clase 04

--- Clase_04.hs
+++ Clase_04.hs
@@ -14,16 +14,18 @@ f1 n = 2^n + f1(n-1)
 
 -- (2) sumatoria sobre i, desde 1 hasta n, de q potencia-i
 
-f2 :: Int -> Int -> Int
+f2 :: Int -> Float -> Float
 f2 0 q = 0
 f2 n q = q^n + f2 (n-1) q 
 
 
 -- (3) sumatoria sobre i, desde 1 hasta 2n, de q potencia-i
 
-f3 :: Int -> Int -> Int 
-f3 n q = f2 (n*2) q 
+f3 :: Int -> Float -> Float
+f3 n q = f2 (2*n) q 
 
 
 -- (4) sumatoria sobre i, desde n hasta 2n, de q potencia-i
 
+f4 :: Int -> Float -> Float
+f4 n q = f3 n q - f2 (n-1) q 
