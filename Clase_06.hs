3bf8fe3566f96ee0d18e20b0dc8d702a3bdee54a
Lucas Achaval <achaval.lucas@gmail.com>
2021-05-17 16:20:53 -0300

    Ejercicio 5 | Clase 06

--- Clase_06.hs
+++ Clase_06.hs
@@ -12,17 +12,29 @@ sumarN :: Int -> [Int] -> [Int]
 sumarN n [] = []
 sumarN n (x:xs) = (x + n) : sumarN n xs
 
+
 -- (3) sumarElPrimero :: [Int] -> [Int] que dada una lista no vacía xs, suma el primer
 -- elemento a cada elemento de xs. Ejemplo sumarElPrimero [1,2,3] [2,3,4]
 
 sumarElPrimero :: [Int] -> [Int]
 sumarElPrimero l = sumarN (head l) l  
 
+
 -- (4) sumarElUltimo :: [Int] -> [Int] que dada una lista no vacía xs, suma el último
 -- elemento a cada elemento de xs. Ejemplo sumarElUltimo [1,2,3] [4,5,6]
 
+sumarElUltimo :: [Int] -> [Int]
+sumarElUltimo l = sumarN (last l) l
+
+
 -- (5) pares :: [Int] -> [Int] que devuelve una lista con los elementos pares de la lista
 -- original. Ejemplo pares [1,2,3,5,8] [2,8]
+
+pares :: [Int] -> [Int]
+pares [] = []
+pares (x:xs) | even x = x : pares xs
+             | otherwise = pares xs   
+
 
 -- (6) quitar :: Int -> [Int] -> [Int] que elimina la primera aparición del elemento en la
 -- lista (de haberla).
