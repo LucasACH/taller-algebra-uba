c32e7c0d29b8752460c7e41c5418897f36fcc3df
Lucas Achaval <achaval.lucas@gmail.com>
2021-05-17 22:45:33 -0300

    Ejercicio 14 y 15 | Clase 06

--- Clase_06.hs
+++ Clase_06.hs
@@ -4,8 +4,8 @@ pertenece n l | l == [] = False
 
 cantidadDeApariciones :: Int -> [Int] -> Int
 cantidadDeApariciones n [] = 0
-cantidadDeApariciones n l | n == head l = 1 + cantidadDeApariciones n (tail l)
-                          | otherwise = cantidadDeApariciones n (tail l)
+cantidadDeApariciones n (x:xs) | n == x = 1 + cantidadDeApariciones n (xs)
+                               | otherwise = cantidadDeApariciones n (xs)
 
 
 -- (1) productoria :: [Int] -> Int que devuelve la productoria de los elementos.
@@ -51,8 +51,8 @@ pares (x:xs) | even x = x : pares xs
 
 quitar :: Int -> [Int] -> [Int]
 quitar n [] = []
-quitar n l | cantidadDeApariciones n l > 1 && n == head l = tail l
-           | otherwise = (head l) : quitar n (tail l)
+quitar n l | head l == n = tail l
+           | otherwise = head l : (quitar n (tail l))
 
 
 -- (7) quitarTodas :: Int -> [Int] -> [Int] que elimina todas las apariciones del elemento
@@ -61,7 +61,7 @@ quitar n l | cantidadDeApariciones n l > 1 && n == head l = tail l
 quitarTodas :: Int -> [Int] -> [Int]
 quitarTodas n [] = []
 quitarTodas n l | cantidadDeApariciones n l > 0 && n == head l = quitarTodas n (tail l)
-                | otherwise = (head l) : quitarTodas n (tail l)
+                     | otherwise = (head l) : quitarTodas n (tail l)
 
 
 -- (8) hayRepetidos :: [Int] -> Bool que indica si una lista tiene elementos repetidos.
@@ -93,6 +93,40 @@ maximo :: [Int] -> Int
 maximo (x:[]) = x
 maximo (x:xs) | x > head xs = maximo (x : tail xs)
               | otherwise = maximo xs
-             
 
-             
+
+-- (12) ordenar :: [Int] -> [Int] que ordena los elementos de forma creciente.
+
+ordenar :: [Int] -> [Int]
+ordenar [] = []
+ordenar l = max : (ordenar (quitarTodas (max) l))
+          where
+              max = maximo l
+
+
+-- (13) reverso :: [Int] -> [Int] que dada una lista invierte su orden.
+
+reverso :: [Int] -> [Int]
+reverso [] = []
+reverso (x:xs) = reverso xs ++ [x]
+
+
+-- (14) concatenar :: [Int] -> [Int] -> [Int] que devuelve la concatenación de la primera
+-- lista con la segunda. Ejemplo concatenar [1,2,3] [4,5,6] [1,2,3,4,5,6],
+-- concatenar [] [4,5,6] [4,5,6]. Esta operación está en el prelude y se escribe como
+-- (++).
+
+concatenar :: [Int] -> [Int] -> [Int]
+concatenar a b = a ++ b
+
+-- (15) zipi :: [a] -> [b] -> [(a,b)] que devuelve una lista de tuplas, cada tupla contiene
+-- elementos de ambas listas que ocurren en la misma posición. En caso que tengan distintas
+-- longitudes, la longitud de la lista resultado es igual a la longitud de la lista más chica
+-- pasada por parámetro. Ejemplo zipi [1,2,3] ['a','b','c']
+-- [(1,'a'), (2,'b'), (3,'c')], zipi [1,2,3] ['a','b'] [(1,'a'), (2,'b')]. Esta
+-- operación está en el prelude y se escribe como zip.
+
+zipi :: [a] -> [b] -> [(a,b)]
+zipi [] b = []
+zipi a [] = []
+zipi (a:as) (b:bs) = (a,b) : (zipi (as) (bs)) 
