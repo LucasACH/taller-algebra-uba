919b6041f1ee0c35b99c2d8ac24c69be72c7e715
Lucas Achaval <achaval.lucas@gmail.com>
2021-06-01 17:07:02 -0300

    Ejercicio 2 | Clase 08

--- Clase_08.hs
+++ Clase_08.hs
@@ -1,6 +1,19 @@
+type Set a = [a]
+
 factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n-1) 
+
+vacio :: Set a
+vacio = []
+
+agregar :: Eq a => a -> Set a -> Set a
+agregar n c | n `elem` c = c
+            | otherwise = n:c
+
+union :: Eq a => Set a -> Set a -> Set a
+union [] ys     = ys
+union (x:xs) ys = union xs (agregar x ys)
 
 
 -- (1) Escribir una función que dados n, k ∈ N tal que 0 ≤ k ≤ n, compute el combinatorio (n | k)
@@ -15,3 +28,17 @@ combinatorio' n 0 = 1
 combinatorio' n k | n == k = 1
                   | otherwise = (combinatorio' (n - 1) k) + (combinatorio' (n - 1) (k - 1))
 
+
+-- (2) Implementar una función variaciones :: Set Int -> Int -> Set [Int] que dado un conjunto c y una longitud k genere todas las posibles listas de longitud k a partir de elementos de c.
+
+agregarElementoAdelante :: Int -> Set [Int] -> Set [Int]
+agregarElementoAdelante x [      ] = []
+agregarElementoAdelante x (ys:yss) = agregar (x:ys) (agregarElementoAdelante x yss)
+
+agregarElementosAListas :: Set Int -> Set [Int] -> Set [Int]
+agregarElementosAListas [    ] _ = []
+agregarElementosAListas (x:xs) c = (agregarElementoAdelante x c) `union` (agregarElementosAListas xs c)
+
+variaciones :: Set Int -> Int -> Set [Int]
+variaciones c 0 = [[]]
+variaciones c k = agregarElementosAListas c (variaciones c (k - 1))
